436bb0518cdb5e505bf492b754bfa7cec20811b6
valyya <valyya@ukr.net>
2019-09-23 19:37:35 +0300

    added section product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+	// фильтр продукции
+	var mixer = mixitup('.products__inner');
+
+//  star отзывы
+	$(".rate-star").rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true
+	});
+
+	$('.product-slider__inner').slick({
+		dots: true,
+		arrows:false,
+		slidesToShow: 4,
+		slidesToScroll: 2
+	});
+	
 
 });
