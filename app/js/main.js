d33d3de2a6b18375484603628a7a2b20e4221d4d
valyya <valyya@ukr.net>
2019-10-02 19:47:14 +0300

    added section new-items01

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,4 @@
 $(function() {
-
-	// фильтр продукции
-	var mixer = mixitup('.products__inner');
-
 //  star отзывы
 	$(".rate-star").rateYo({
 		rating: 5,
@@ -10,12 +6,34 @@ $(function() {
 		readOnly: true
 	});
 
+
+
+	// !!!должен быть перед // фильтр продукции mixitup
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+
+
+	// фильтр продукции
+	var mixer = mixitup('.products__inner');
+
+
 	$('.product-slider__inner').slick({
 		dots: true,
 		arrows:false,
 		slidesToShow: 4,
 		slidesToScroll: 2
 	});
+
+
+
+	
 	
 
 });
